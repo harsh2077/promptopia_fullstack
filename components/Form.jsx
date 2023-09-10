f467dc2fae7da300ae1b418c72cb05597b2029d8
harsh2077 <110387163+harsh2077@users.noreply.github.com>
2023-09-10 17:03:19 +0530

    Update Form.jsx

--- components/Form.jsx
+++ components/Form.jsx
@@ -5,7 +5,6 @@ import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
 
 import Form from "@components/Form";
-
 const CreatePrompt = () => {
   const router = useRouter();
   const { data: session } = useSession();
